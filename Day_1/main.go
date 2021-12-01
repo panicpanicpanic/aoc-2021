c29c836dfba9c1b796c4b9f98b1a614893d2e1d0
Angel Martinez <angel.martinez@hinge.co>
2021-12-01 19:14:19 +0000

    day1: refactor with logic for part 2

--- Day_1/main.go
+++ Day_1/main.go
@@ -9,6 +9,7 @@ import (
 )
 
 func main() {
+	// open input text file
 	f, err := os.Open("input.txt")
 	if err != nil {
 		log.Fatal(err)
@@ -17,14 +18,19 @@ func main() {
 
 	scanner := bufio.NewScanner(f)
 
+	// part 1: return the # of increased measurements
 	var increasedMeasurements int32
 	var lastMeasurement int32
+	var measurements []int32
 
 	for scanner.Scan() {
 		measurement, err := strconv.ParseInt(scanner.Text(), 10, 32)
 		if err != nil {
 			log.Fatal(err)
 		}
+
+		// used for part 2
+		measurements = append(measurements, int32(measurement))
 
 		if lastMeasurement == 0 {
 			lastMeasurement = int32(measurement)
@@ -41,5 +47,29 @@ func main() {
 		log.Fatal(err)
 	}
 
-	log.Print(increasedMeasurements)
+	log.Printf("part 1: # of increased measurements: %d \n", increasedMeasurements)
+
+	// part 2: return the number of three-measurement sums larger than previous sums
+	var bucketSum, lastBucketSum, increasedSum int32
+	for i := range measurements {
+		if lastBucketSum == 0 {
+			for _, r := range measurements[i : i+3] {
+				lastBucketSum += r
+			}
+			continue
+		}
+
+		for _, r := range measurements[i : i+3] {
+			bucketSum += r
+		}
+
+		if bucketSum > lastBucketSum {
+			increasedSum++
+		}
+
+		lastBucketSum = bucketSum
+		bucketSum = 0
+	}
+
+	log.Printf("part 2: # of sliding window increased measurements: %d \n", increasedSum)
 }
