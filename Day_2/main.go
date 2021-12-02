410d2dce52a9a0750ce26df3bb5d48c0c1497634
Angel Martinez <angel.martinez@hinge.co>
2021-12-02 16:52:41 +0000

    day2: add part 1 solution

--- Day_2/main.go
+++ Day_2/main.go
@@ -1,4 +1,47 @@
 // https://adventofcode.com/2021/day/2
 package main
 
-func main() {}
+import (
+	"bufio"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	// open input text file
+	f, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	// x/y positioning
+	var x, y int32
+
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		instructions := strings.Split(scanner.Text(), " ")
+
+		direction := instructions[0]
+		magnitude, err := strconv.ParseInt(instructions[1], 10, 32)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		switch {
+		case direction == "forward":
+			x += int32(magnitude)
+		case direction == "up":
+			y -= int32(magnitude)
+		case direction == "down":
+			y += int32(magnitude)
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("part 1: x-coordinate: %d y-coordinate: %d product: %d \n", x, y, x*y)
+}
